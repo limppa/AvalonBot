b73c048e3f19e3c26cd114c96597ff5f557140ea
Axel Neergaard <axel.neergaard@cujo.com>
2023-08-02 16:02:25 +0300

    fix: ensure same song is not played twice in a row

--- AvalonBot 1.4.6.py
+++ AvalonBot 1.4.6.py
@@ -85,6 +85,8 @@ quotes_file = "text/quotes.txt"
 
 ####### AUDIO #########
 
+current_song = None
+
 def play_sound(file_name):
     file_path = 'sounds/' + file_name
     pg.mixer.music.load(file_path)
@@ -107,6 +109,8 @@ def loop_music(folder_name, file_name):
 
 # plays random song on loop
 def shuffle_folder(folder_name):
+    global current_song
+
     folder_path = 'music/' + folder_name
     if not os.path.exists(folder_path):
         print(f"Error: Folder '{folder_name}' not found in 'music'.")
@@ -119,6 +123,9 @@ def shuffle_folder(folder_name):
         return
 
     random_song = random.choice(mp3_files)
+    while random_song == current_song:
+        random_song = random.choice(mp3_files)
+    current_song = random_song
     #file_path = os.path.join(folder_path, random_song)
     #loop_music2(file_path)
     loop_music(folder_name, random_song)
